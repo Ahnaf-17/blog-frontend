5a61ca45789e1aee5adea5baa9fa197b1a943c88
unknown <ahnaf.ahmed.42173@gmail.com>
2023-11-05 23:33:35 +0600

    all blogs viewed

--- src/Pages/AllBlog/AllBlog.jsx
+++ src/Pages/AllBlog/AllBlog.jsx
@@ -1,12 +1,15 @@
 import { useLoaderData } from "react-router-dom";
+import BlogCard from "./BlogCard";
 
 const AllBlog = () => {
     const blog = useLoaderData()
     return (
-        <div>
+        <div >
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mx-14 my-16 ">
             {
-                blog.length
+                blog.map(blog => <BlogCard key={blog.id} blog={blog}></BlogCard>)
             }
+            </div>
         </div>
     );
 };
--- src/Pages/AllBlog/BlogCard.jsx
+++ src/Pages/AllBlog/BlogCard.jsx
@@ -1,17 +1,34 @@
 
-const BlogCard = () => {
+const BlogCard = ({ blog }) => {
+    const { title, image, short_description } = blog;
     return (
-        <div className="card lg:card-side bg-base-100 shadow-xl">
-  <figure><img src="/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album"/></figure>
-  <div className="card-body">
-    <h2 className="card-title">New album is released!</h2>
-    <p>Click the button to listen on Spotiwhy app.</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Listen</button>
-    </div>
-  </div>
-</div>
+        // <div className="card  lg:card-side bg-base-100 shadow-xl">
+        //     <figure ><img className="" src={image} alt="Album" /></figure>
+        //     <div className="card-body">
+        //         <h2 className="card-title">{title}</h2>
+        //         <p>{short_description}</p>
+        //         <div className="card-actions justify-end">
+        //             <button className="btn btn-primary">Listen</button>
+        //         </div>
+        //     </div>
+        // </div>
+        <div className="card bg-base-100 shadow-xl  lg:flex">
+            <div>
+                <figure><img className="md:h-[300px] rounded-t-xl md:w-full" src={image} alt="Shoes" /></figure>
+            </div>
+            <div className="card-body">
+                <h2 className="card-title">
+                    {title}
+                </h2>
+                <p>{short_description}</p>
+                <div className="card-actions justify-end">
+                    <div className="badge badge-outline">Fashion</div>
+                    <div className="badge badge-outline">Products</div>
+                </div>
+            </div>
+        </div>
     );
 };
 
 export default BlogCard;
+// lg:h-64
