4f64d41d99301406ef123da4a8721483d3dc6fe8
unknown <ahnaf.ahmed.42173@gmail.com>
2023-11-06 20:45:46 +0600

    react photo viewer added to blog details page

--- src/Pages/BlogDetail/BlogDetailPage.jsx
+++ src/Pages/BlogDetail/BlogDetailPage.jsx
@@ -1,23 +1,35 @@
 /* eslint-disable react/prop-types */
+import 'react-photo-view/dist/react-photo-view.css';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
 
 const BlogDetailPage = ({ selectedBlog }) => {
-    const { title, image, short_description, long_description } = selectedBlog || {}
+    const {_id, title, image, short_description, long_description } = selectedBlog || {}
     return (
-        <div>
-            <div className="hero md:min-h-16 bg-base-100">
-                <div className="hero-content flex-col lg:flex-row">
-                    <img src={image} className="md:max-w-xl max-w-sm rounded-lg shadow-2xl" />
-                    <div>
-                        <h1 className="text-4xl font-bold">{title}</h1>
-                        <p className="py-6">{short_description}</p>
+        <PhotoProvider>
+            <div>
+                <div className="hero md:min-h-16 bg-base-100">
 
+                    <div className="hero-content flex-col lg:flex-row">
+                        <div className="foo">
+                            <PhotoView key={_id} src={image}>
+                                <img className="md:max-w-xl max-w-sm rounded-lg shadow-2xl" src={image} alt="" />
+                            </PhotoView>
+                        </div>
+
+                        {/* <img src={image} className="md:max-w-xl max-w-sm rounded-lg shadow-2xl" /> */}
+                        {/* <ReactPhotoView src={image}></ReactPhotoView> */}
+                        <div>
+                            <h1 className="text-4xl font-bold">{title}</h1>
+                            <p className="py-6">{short_description}</p>
+
+                        </div>
                     </div>
                 </div>
+                <div className="mg:px-20 md:py-16 p-5">
+                    <p>{long_description}</p>
+                </div>
             </div>
-            <div className="mg:px-20 md:py-16 p-5">
-                <p>{long_description}</p>
-            </div>
-        </div>
+        </PhotoProvider>
     );
 };
 
